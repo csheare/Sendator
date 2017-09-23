382c558f012359fa88b8c4f4ecbcc2ae85508c51
miguel toledo <toledomiguel5@gmail.com>
2017-09-23 11:51:33 -0400

    nodemailer implemented

--- api/email.js
+++ api/email.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 
-const app = express();
+const router = express.Router()
 
-const transport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'youremail@address.com',
-        pass: 'yourpassword'
-    }
+const sendEmail = (name, state, comment) => {
+    return new Promise((resolve, reject) => {
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'mhackssendator@gmail.com',
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+    
+        const mailOption = {
+            from: '<mhackssendator@gmail.com>',
+            to: 'toledomiguel5@gmail.com',
+            subject: 'hello',
+            text: comment
+        };
+    
+        transport.sendMail(mailOption, (err) => {
+            if (err) {
+                reject(err.message);
+            } else {
+                resolve({status: 200})
+            }
+        })
+    })
+}
+
+router.post('/', (req, res)=> {
+    const {name, comment} = req.body;
+    sendEmail(name, comment)
+    .then(() => { res.redirect('/')})
+    .catch(err => { 
+        console.log(err)
+        res.sendStatus(500)
+    });
 });
 
-app.post('/api/email', (req, res)=> {
-    res.sendStatus(200);
-});
+module.exports = router;
--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const email = require('./api/email');
+
 const url = 'localhost';
 const port = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('index');
 });
 
+app.use('/api/email', email);
 
 app.listen(port, url, ()=> {
     console.log('Server started on localhost', port)
